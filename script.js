59ffbe29a05ceffe497e42d09b192f03201683b1
Bassam <bassamkhamis7@gmail.com>
2022-07-19 05:18:42 -0400

    finished 1 round of the game

--- script.js
+++ script.js
@@ -1,6 +1,70 @@
 //Function To generate a random 
 const actions = ["Rock", "Paper", "Scissors"]
+
 function getComputerChoice(){
     var rand = Math.floor(Math.random()*3)
-    console.log(actions[rand]);
+    return actions[rand];
+}
+
+function playRound(playerSelection, computerSelection){
+    var player = playerSelection.toLowerCase();
+    var computer = computerSelection.toLowerCase();
+    var flag = 0;
+
+    switch(player){
+        case "rock":
+            switch(computer){
+                case "scissors":
+                    flag = 1;
+                    break;
+                case "paper":
+                    flag = 2;
+                    break;
+                default:
+                    flag = 0;
+            }
+            break;
+        case "paper":
+            switch(computer){
+                case "rock":
+                    flag = 1;
+                    break;
+                case "scissors":
+                    flag = 2;
+                    break;
+                default:
+                    flag = 0;
+            }
+            break;
+        case "scissors":
+            switch(computer){
+                case "paper":
+                    flag = 1;
+                    break;
+                case "rock":
+                    flag = 2;
+                    break;
+                default:
+                    flag = 0;
+            }
+            break;
+        default:
+            flag = -1;
+    }
+    switch (flag){
+        case 1:
+            return "you win! " + playerSelection + " beats " + computerSelection;
+            break;
+        case 2:
+            return "you Lose! " + computerSelection + " beats " + playerSelection;
+            break;
+        default:
+            return "TIE " ;
+    }
+}
+
+const playerSelection = "Rock";
+for(let i = 0; i < 5; i++){
+    var computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
 }
